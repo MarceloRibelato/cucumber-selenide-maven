49e407ae810207102d9fd80ce11af234fb31501e
ilja.pavlovs@neotech.lv <Jqwvm6riMxJMQvM>
2018-10-25 14:42:49 +0300

    Update

--- src/test/java/lv/iljapavlovs/cucumber/pageobjects/google/GooglePage.java
+++ src/test/java/lv/iljapavlovs/cucumber/pageobjects/google/GooglePage.java
@@ -1,18 +1,27 @@
 package lv.iljapavlovs.cucumber.pageobjects.google;
 
 
+import static com.codeborne.selenide.Selenide.page;
+
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.support.FindBy;
 
-import static com.codeborne.selenide.Selenide.page;
-
 public class GooglePage {
 
+    //Classic PO to work with PageFactory - https://selenide.org/documentation/page-objects.html
     @FindBy(name = "q")
     private SelenideElement inputField;
 
+    //OR
+
+    //    SIMPLE
+//    private SelenideElement inputField = $("[name='q']") ;
+
+
     public GoogleSearchResultPage searchFor(String textToSearchFor) {
         inputField.val(textToSearchFor).pressEnter();
+
+        // No need in PageFactory.initElements()
         return page(GoogleSearchResultPage.class);
     }
 
